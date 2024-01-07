1faba47a7953127d338f3c3cada6c4b930226f5f
Paulina Siwko <pau.siwko@gmail.com>
2024-01-07 20:29:11 +0100

    added localStorage to save user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,21 @@ function initialisePage() {
     setCurrentDay();
 
     const timetableEl = $("#timetable");
+    const userInput = JSON.parse(localStorage.getItem("hourText")) || [];
 
-    for (var i = 10; i < 21; i++) {
+    for (var i = 9; i < 21; i++) {
+        var rowObject = userInput.find((element) => element.hour === i);
+        var text = "";
+        if (rowObject != null || rowObject != undefined) {
+            text = rowObject.text;
+        }
+
         if (currentHour - i > 0) {
-            timetableEl.append(createRow(i, "", currentHour).addClass("past"));
+            timetableEl.append(createRow(i, text).addClass("past"));
         } else if (currentHour - i == 0) {
-            timetableEl.append(createRow(i, "", currentHour).addClass("present")); 
+            timetableEl.append(createRow(i, text).addClass("present")); 
         } else {
-            timetableEl.append(createRow(i, "", currentHour).addClass("future"));
+            timetableEl.append(createRow(i, text).addClass("future"));
         }
     }
 }
@@ -31,6 +38,24 @@ function createRow(rowHour, savedText) {
 
     var saveIcon = $("<i>").addClass("fas fa-save");
 
+    btnCol.on("click", function(e) {
+        e.preventDefault();
+
+        const hourText = JSON.parse(localStorage.getItem("hourText")) || [];
+
+        var a = hourText.find((element) => element.hour === rowHour);
+
+        if (a != undefined) {
+            a.text = textCol.val();
+        } else {
+            hourText.push({
+                hour: rowHour,
+                text: textCol.val()
+            })
+        }
+
+        localStorage.setItem("hourText", JSON.stringify(hourText));
+    })
 
     newRow.append(hourCol, textCol, btnCol);
     btnCol.append(saveIcon);
